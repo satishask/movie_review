36e9833b62eeb07a8a101ad5391a2cb4cfa8e3e8
Satish ASK <satishselvan@gmail.com>
2017-10-17 22:39:23 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MovieReview::Application.config.secret_token = '3ef182a2f009e66c9ee1182d639ce09fc12d4e129a13da4817a87e8f8ad2832424d58cea4d9ffba273df587584137a68cbdd7b2496e5c3dc3561c331d7626f6c'
